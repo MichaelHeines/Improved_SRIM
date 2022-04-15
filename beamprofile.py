08f2bf7c18c5629c4254752410c9a6699fa15692
MichaelHeines <84977146+MichaelHeines@users.noreply.github.com>
2022-04-15 14:02:02 +0200

    Update beamprofile.py

--- beamprofile.py
+++ beamprofile.py
@@ -4,18 +4,22 @@ import numpy as np
 import os.path
 
 # This program generates a realistic beam profile for SRIM .dat input
+
+# Define beam parameter class
 class beam_parameters:
-    def __init__(self, y_offset, z_offset, y_std, z_std, nr_particles, atomic_number, energy, energy_std_fractional):
-        self.y_offset = y_offset
-        self.z_offset = z_offset
-        self.y_std = y_std
-        self.z_std = z_std
-        self.nr_particles = nr_particles
-        self.atomic_number = atomic_number
+    def __init__(self, y_offset, z_offset, y_std, z_std, angle, nr_particles, atomic_number, energy, energy_std_fractional):
+        self.y_offset = y_offset                                # Y offset of beam spot
+        self.z_offset = z_offset                                # Z offset of beam spot
+        self.y_std = y_std                                      # standard deviation of the beam in the Y direction
+        self.z_std = z_std                                      # standard deviation of the beam in the Z direction
+        self.angle = angle                                      # angle of implantation
+        self.nr_particles = nr_particles                        # number of particles simulated
+        self.atomic_number = atomic_number                      # Atomic number of simulated particles
         self.energy = energy                                    # Requested energy
-        self.energy_std_fractional = energy_std_fractional      # Energy deviation (not properly implemented)
+        self.energy_std_fractional = energy_std_fractional      # Energy deviation (not properly implemented yet)
 
-
+        
+# Generate informative text about dat file
 def generate_toptext(string_dat):
     line1 = "=========== TRIM with various Incident Ion Energies/Angles and Depths ========= \n"
     line2 = "= This file tabulates the kinetics of incident ions or atoms.                 = \n"
@@ -32,6 +36,7 @@ def generate_toptext(string_dat):
     return text_dat
 
 
+# Generate 5 char number strings
 def generate_numbers(nr_particles):
     N_space = np.linspace(1, nr_particles, nr_particles)
     N_string = []
@@ -52,21 +57,24 @@ def generate_dat(beam_parameters, save_path, file_name):
     z_offset = beam_parameters.z_offset
     y_std = beam_parameters.y_std
     z_std = beam_parameters.z_std
+    angle = beam_parameters.angle
     nr_particles = beam_parameters.nr_particles
     atomic_number = beam_parameters.atomic_number
     energy = beam_parameters.energy
     energy_std_fractional = beam_parameters.energy_std_fractional
 
     complete_name_out = os.path.join(save_path, file_name)
-
+    
+    # Get an energy and position
     Ion_number = generate_numbers(nr_particles)
     E = np.random.normal(energy, energy*energy_std_fractional, nr_particles)
     x = np.zeros(nr_particles)
     y = np.random.normal(y_offset, y_std, nr_particles)
     z = np.random.normal(z_offset, z_std, nr_particles)
     
-    phi = np.random.uniform(0, 2 * math.pi, nr_particles)
-    theta = np.zeros(nr_particles)
+    # Get a beam direction
+    phi = np.zeros(nr_particles)                                    # np.random.uniform(0, 2 * math.pi, nr_particles)
+    theta = angle * np.ones(nr_particles)
     x_vec = np.cos(theta)                                           # x in SRIM is beam direction --> theta azimuthal x angle
     y_vec = np.cos(phi) * np.sin(theta)
     z_vec = np.sin(phi) * np.sin(theta)
@@ -75,6 +83,7 @@ def generate_dat(beam_parameters, save_path, file_name):
     text = generate_toptext("Beam profile" + file_name)
     g.write(text)
     
+    # Write out the particle properties
     for i in range(nr_particles):
         line = str(Ion_number[i]) + "\t" + str(atomic_number) + "\t" + str("{:e}".format(E[i]) + "\t" + str("{:e}".format(x[i])) + "\t" + str("{:e}".format(y[i])) + "\t" + str("{:e}".format(z[i])) + "\t" + str("{:e}".format(x_vec[i])) + "\t" + str("{:e}".format(y_vec[i])) + "\t" + str("{:e}".format(z_vec[i])) + "\n")
         g.write(line)
@@ -82,7 +91,7 @@ def generate_dat(beam_parameters, save_path, file_name):
 
 
 # Inputs and default values
-parameters = beam_parameters(0, 0, 0, 0, 10**5, 19, 60 * 10**3, 0)
+parameters = beam_parameters(0, 0, 0, 0, 0, 10**5, 19, 60 * 10**3, 0)
 save_path = '/mnt/ksf9/H2/user/u0148069/share/my.public/SRIM_scripts/beam_profiles/'
 file_name = 'Potassium_60keV.txt'
 
